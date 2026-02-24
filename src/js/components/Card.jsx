43dded410462d22330bf78ec506e0fe12f68f818
Sean W. Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2026-02-24 15:50:43 +0000

    set up card component

--- src/js/components/Card.jsx
+++ src/js/components/Card.jsx
@@ -1,3 +1,16 @@
-export const Card = () => {
-    
-}
+export const Card = ([cardTitle, cardDescription]) => {
+  return (
+    <div className="card h-100">
+      <img src="https://picsum.photos/id/866/200/200" className="card-img-top" alt="..." />
+      <div className="card-body">
+        <h5 className="card-title">{cardTitle}</h5>
+        <p className="card-text">
+          {cardDescription}
+        </p>
+        <a href="#" className="btn btn-primary">
+          Find out more!
+        </a>
+      </div>
+    </div>
+  );
+};
